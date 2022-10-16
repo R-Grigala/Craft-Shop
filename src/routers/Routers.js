02691072f0f2edba9ac8ead786c0cc32c129ab3f
R-Grigala <r.grigalashvili777@gmail.com>
2022-10-16 20:14:30 +0400

    Coding Pause

---
+++ src/routers/Routers.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Shop from '../pages/Shop'
+import Cart from '../pages/Cart'
+import ProductDetails from '../pages/ProductDetails'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import Singup from '../pages/Singup'
+
+const Routers = () => {
+  return (
+  <Routes>
+    <Route path='home' element={<Home/>}/>
+    <Route path='shop' element={<Shop/>}/>
+    <Route path='shop/:id' element={<ProductDetails/>}/>
+    <Route path='cart' element={<Cart/>}/>
+    <Route path='checkout' element={<Checkout/>}/>
+    <Route path='login' element={<Login/>}/>
+    <Route path='singup' element={<Singup/>}/>
+  </Routes>
+  );
+}
+
+export default Routers
