79981f5dc8e9d59983b81d91c5ab447c5ee19d93
R-Grigala <r.grigalashvili777@gmail.com>
2022-11-05 21:29:33 +0400

    Coding Continue

---
+++ src/services/Services.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import { motion } from 'framer-motion'
+
+const Services = () => {
+  return <section className='services'>
+    <Container>
+        <Row>
+            <Col lg='3' md='4'>
+                <div className="sercice__item">
+                    <span></span>
+                </div>
+            </Col>
+        </Row>
+    </Container>
+  </section>
+}
+
+export default Services
