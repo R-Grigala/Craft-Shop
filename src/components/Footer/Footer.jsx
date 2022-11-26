0dcb7e52900b5ef4e5082d1dff2c05da2d07bc54
R-Grigala <r.grigalashvili777@gmail.com>
2022-11-26 20:06:32 +0400

    Coding Continue : // Finished Part 1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return <footer className='footer'>
     <Container>
       <Row>
-        <Col lg='4'>
+        <Col lg='4' className='mb-4' md='6'>
           <div className='logo'>
             <div>
               <h1 className='text-white'>Multimart</h1>
@@ -23,7 +23,7 @@ const Footer = () => {
           </p>
         </Col>
 
-        <Col lg='3'>
+        <Col lg='3' md='3' className='mb-4'>
           <div className="footer__quick-links">
             <h4 className='quick__links-title'>Top Categories</h4>
             <ListGroup className='mb-3'>
@@ -46,7 +46,7 @@ const Footer = () => {
           </div>
         </Col>
 
-        <Col lg='2'>
+        <Col lg='2' md='3' className='mb-4'>
         <div className="footer__quick-links">
             <h4 className='quick__links-title'>Useful Links</h4>
             <ListGroup className='mb-3'>
@@ -69,7 +69,7 @@ const Footer = () => {
           </div>
         </Col>
 
-        <Col lg='3'>
+        <Col lg='3' md='4'>
         <div className="footer__quick-links">
             <h4 className='quick__links-title'>Contact</h4>
             <ListGroup className='footer__contact'>
