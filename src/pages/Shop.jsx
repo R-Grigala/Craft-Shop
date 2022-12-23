ff25b9561b0e381ac9a81cc0577bda87e67b15a0
R-Grigala <r.grigalashvili777@gmail.com>
2022-12-24 03:22:33 +0400

    Coding Continue

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
 
 const Shop = () => {
-  return (
-    <div>Shop</div>
-  )
-}
+  return(
+    <Helmet title="Shop">
+      <CommonSection title='Products'/>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3' md='3'>
+              <div className="filter__widget">
+                <select>
+                <option>Filter By Category</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='3' md='3'>
+              <div className="filter__widget">
+                <select>
+                <option>Sort By</option>
+                  <option value="ascending">Ascending</option>
+                  <option value="descending">Descending</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='6' md='6'>
+              <div className="search__box">
+                <input type="text" placeholder="Search......" />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    
+    </Helmet>
+  );
+};
 
 export default Shop
