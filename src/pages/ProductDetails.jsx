a8a77183a77aea9fd78540ee090de46e9e14f3e8
R-Grigala <r.grigalashvili777@gmail.com>
2022-12-24 22:22:03 +0400

    Coding Continue:// 53:16 Part 2

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -5,17 +5,20 @@ import { useParams } from 'react-router-dom';
 import products from '../assets/data/products';
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/CommonSection';
+import { motion } from 'framer-motion';
+
+import '../styles/product-details.css'
 
 const ProductDetails = () => {
 
   const {id} = useParams()
   const product = products.find(item => item.id === id)
 
-  const {imgUrl, productName, price, avgRating, review, description, shortDesc } = product
+  const {imgUrl, productName, price, avgRating, reviews, description, shortDesc } = product
   
   return (
-    <Helmet>
-      <CommonSection/>
+    <Helmet title={productName}>
+      <CommonSection title={productName}/>
 
       <section className='pt-0'>
         <Container>
@@ -27,7 +30,7 @@ const ProductDetails = () => {
             <Col lg='6'>
               <div className="product__details">
                 <h2>{productName}</h2>
-                <div className='product__raiting'>
+                <div className='product__rating d-flex align-items-center gap-5 mb-3'>
                   <div>
                     <span>
                       <i class="ri-star-fill"></i>
@@ -46,12 +49,30 @@ const ProductDetails = () => {
                     </span>
                   </div>
 
-                  <p>({avgRating} ratings)</p>
+                  <p>
+                    (<span>{avgRating}</span> ratings)
+                  </p>
                 </div>
-                <span>{price}</span>
-                <p>{shortDesc}</p>
+                <span className='product__price'>${price}</span>
+                <p className='mt-3'>{shortDesc}</p>
 
-                <button className="buy__btn">Add to Cart</button>
+                <motion.button whileTap={{ scale:1.2 }} className="buy__btn">Add to Cart</motion.button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <div className="tab__wrapper d-flex align-items-center gap-5">
+                <h6>Description</h6>
+                <h6>Reviews ({reviews.length})</h6>
+              </div>
+              <div className="tab__content mt-5">
+                <p>{description}</p>
               </div>
             </Col>
           </Row>
