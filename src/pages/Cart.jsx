a54d83f504e342a0059a88eb1be5ab5fa4b64bae
R-Grigala <r.grigalashvili777@gmail.com>
2023-01-17 17:16:04 +0400

    Coding Continue :// 1:29:33 Part 2

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,11 +2,44 @@ import React from 'react';
 import '../styles/cart.css';
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/CommonSection';
+import { Container, Row, Col } from 'reactstrap';
+
+import tdImg from '../assets/images/arm-chair-01.jpg'
 
 const Cart = () => {
   return (
     <Helmet title>
       <CommonSection title="Shopping Cart"/>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='9'>
+              <table className='table bordered'>
+                <thead>
+                  <tr>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Qty</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td><img src={tdImg} alt=''/></td>
+                    <td>Modern Arm Chair</td>
+                    <td>$299</td>
+                    <td>2px</td>
+                    <td><i className="ri-delete-bin-line"></i></td>
+                  </tr>
+                </tbody>
+              </table>
+            </Col>
+            <Col lg='3'></Col>
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   )
 }
