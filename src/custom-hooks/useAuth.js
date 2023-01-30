f4a7aff55638c7fc364f4c8397fcb6f258397655
R-Grigala <r.grigalashvili777@gmail.com>
2023-01-30 15:11:39 +0400

    Coding Continue :// 44:00 Part 3

---
+++ src/custom-hooks/useAuth.js
@@ -0,0 +1,24 @@
+import {useState, useEffect} from 'react'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+
+const useAuth = () => {
+
+    const [currentUser, setCurrnetUser] = useState({});
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                setCurrnetUser(user);
+            } else {
+                setCurrnetUser(null);
+            }
+        });
+    });
+
+    return {
+        currentUser,
+    };
+};
+
+export default useAuth;
