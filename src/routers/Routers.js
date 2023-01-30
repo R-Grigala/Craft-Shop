c8eb17ba9b177c8324246aec4f8e2fe7e71420d6
R-Grigala <r.grigalashvili777@gmail.com>
2023-01-30 15:22:45 +0400

    Coding Continue:// 46:13 Part 3

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from '../pages/Home'
-import Shop from '../pages/Shop'
-import Cart from '../pages/Cart'
-import ProductDetails from '../pages/ProductDetails'
-import Checkout from '../pages/Checkout'
-import Login from '../pages/Login'
-import Singup from '../pages/Singup'
+import Home from '../pages/Home';
+import Shop from '../pages/Shop';
+import Cart from '../pages/Cart';
+import ProductDetails from '../pages/ProductDetails';
+import Checkout from '../pages/Checkout';
+import Login from '../pages/Login';
+import Singup from '../pages/Singup';
+import ProtectedRoute from './ProtectedRoute';
 
 const Routers = () => {
   return (
@@ -17,7 +18,14 @@ const Routers = () => {
     <Route path='shop' element={<Shop/>}/>
     <Route path='shop/:id' element={<ProductDetails/>}/>
     <Route path='cart' element={<Cart/>}/>
-    <Route path='checkout' element={<Checkout/>}/>
+    <Route 
+      path='checkout' 
+      element={
+        <ProtectedRoute>
+          <Checkout />
+        </ProtectedRoute>
+      }
+    />
     <Route path='login' element={<Login/>}/>
     <Route path='singup' element={<Singup/>}/>
   </Routes>
