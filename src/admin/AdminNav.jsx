2e9f550a4858abef5a88fbb0349ed1d2dec24893
R-Grigala <r.grigalashvili777@gmail.com>
2023-02-03 16:57:04 +0400

    Coding Continue:// 20:41 Part 4

--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -1,22 +1,80 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row } from 'reactstrap';
+import useAuth from '../custom-hooks/useAuth';
+import '../styles/admin-nav.css';
+
+import { NavLink } from 'react-router-dom';
+
+const admin__nav = [
+    {
+        display:'Dashboard',
+        path:'/dashboard'
+    },
+    {
+        display:'All-Products',
+        path:'/dashboard/all-products'
+    },
+    {
+        display:'Orders',
+        path:'/dashboard/orders'
+    },
+    {
+        display:'Users',
+        path:'/dashboard/users'
+    }
+]
 
 const AdminNav = () => {
-    return <header className='admin__header'>
-        <div className="admin__nav-top">
+
+    const {currentUser} = useAuth()
+
+    return (
+        <>
+        <header className='admin__header'>
+            <div className="admin__nav-top">
+                <Container>
+                    <div className='admin__nav-wrapper-top'>
+                        <div className="logo">
+                            <h2>Multimart</h2>
+                        </div>
+                        <div className="search__box">
+                            <input type='text' placeholder='Search....'/>
+                            <span><i className='ri-search-line'></i></span>
+                        </div>
+                        <div className='admin__nav-top-right'>
+                            <span>
+                                <i className="ri-notification-3-line"></i>
+                            </span>
+                            <span>
+                                <i className="ri-settings-2-line"></i>
+                            </span>
+                            <img src={currentUser.photoURL} alt="can't"/>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        </header>
+
+        <section className='admin__menu p-0'>
             <Container>
-                <div className='admin__nav-wrapper-top'>
-                    <div className="logo">
-                        <h2>Multimart</h2>
+                <Row>
+                    <div className="admin__navigation">
+                        <ul className="admin__menu-list">
+                            {
+                                admin__nav.map((item, index)=> (
+                                    <li className="admin__menu-item" key={index}>
+                                        <NavLink to={item.path}>{item.display}</NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
                     </div>
-                    <div className="search__box">
-                        <input type='text' placeholder='Search....'/>
-                        <span><i className='ri-search-line'></i></span>
-                    </div>
-                </div>
+                </Row>
             </Container>
-        </div>
-    </header>
+        </section>
+
+        </>
+    );
 };
 
 export default AdminNav;
