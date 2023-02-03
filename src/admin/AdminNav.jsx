8888e513fe66e3b71cf9cf2838c59e5f5f851e6e
R-Grigala <r.grigalashvili777@gmail.com>
2023-02-03 16:00:00 +0400

    Coding Continue: // 9:53 Part 4

--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 const AdminNav = () => {
-  return (
-    <div>AdminNav</div>
-  )
-}
+    return <header className='admin__header'>
+        <div className="admin__nav-top">
+            <Container>
+                <div className='admin__nav-wrapper-top'>
+                    <div className="logo">
+                        <h2>Multimart</h2>
+                    </div>
+                    <div className="search__box">
+                        <input type='text' placeholder='Search....'/>
+                        <span><i className='ri-search-line'></i></span>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    </header>
+};
 
 export default AdminNav;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,11 +5,17 @@ import Footer from '../Footer/Footer';
 import Routers from '../../routers/Routers';
 
 import AdminNav from '../../admin/AdminNav';
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
+
+  const location = useLocation()
+
   return (
    <>
-    <Header />
+
+   {location.pathname.startsWith('/dashboard') ? <AdminNav/> : <Header />}
+   
     <div>
         <Routers />
     </div>
